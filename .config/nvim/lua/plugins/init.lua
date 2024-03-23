2f5ce6ebe79c851ece9b67bcb55c0701b2ddef91
Acol <panagera@gmail.com>
2024-03-23 02:36:31 +0200

    init.lua small fix

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -68,12 +68,6 @@ return {
     },
   },
   {
-    "mfussenegger/nvim-dap",
-    config = function(_, _)
-      require("core.utils").load_mappings "dap"
-    end,
-  },
-  {
     "neovim/nvim-lspconfig",
     config = function()
       require("nvchad.configs.lspconfig").defaults()
